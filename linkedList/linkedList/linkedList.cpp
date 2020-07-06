6133a5367260f65f3a659f87b068f778214d63eb
iam-Abol <mh.z77w@gmail.com>
2020-07-06 18:21:06 +0430

    link lish Implemented with template

--- linkedList/linkedList/linkedList.cpp
+++ linkedList/linkedList/linkedList.cpp
@@ -1,12 +1,13 @@
 #include "stdafx.h"
 #include <iostream>
-
+#include <string>
+template <class T>
 class Node{
 public:
-	int element;
+	T element;
 	Node *next;
 
-	Node(int data){
+	Node(T data){
 		this->element = data;
 	}
 
@@ -18,12 +19,13 @@ public:
 	}
 
 };
+template <class T>
 class LinkedList{
-	Node *head = new Node;
+	Node<T> *head = new Node<T>;
 
 	int size = 0;
 protected:
-	void addRecursive(Node *temp, Node *node){
+	void addRecursive(Node<T> *temp, Node<T> *node){
 		if (!temp->next){
 			temp->next = node;
 		}
@@ -32,7 +34,7 @@ protected:
 		}
 		size++;
 	}
-	void recursiveRemove(Node *temp){
+	void recursiveRemove(Node<T> *temp){
 		if (!temp->next->next){
 			temp->next = NULL;
 		}
@@ -42,9 +44,9 @@ protected:
 		size--;
 	}
 public:
-	void insertLast(Node *newNode){
+	void insertLast(Node<T> *newNode){
 
-		Node *t;
+		Node<T> *t;
 		if (size == 0){
 			head = newNode;
 			size++;
@@ -61,7 +63,7 @@ public:
 
 
 	}
-	void insertAfter(int after, Node *node){
+	void insertAfter(int after, Node<T> *node){
 		if (after > size){
 			std::cout << "list size is short" << std::endl;
 		}
@@ -70,7 +72,7 @@ public:
 			size++;
 		}
 		else{
-			Node *temp;
+			Node<T> *temp;
 			temp = head;
 			int i = 1;
 			while (i < after){
@@ -85,9 +87,9 @@ public:
 		}
 	}
 	void reverse(){
-		Node *current = head;
-		Node* next = NULL;
-		Node *preview = NULL;
+		Node<T> *current = head;
+		Node<T>* next = NULL;
+		Node<T> *preview = NULL;
 		if (size == 1){
 			std::cout << "list has 1 element" << std::endl;
 		}
@@ -103,7 +105,7 @@ public:
 		}
 	}
 	void remove(){
-		Node *temp;
+		Node<T> *temp;
 
 		if (size == 0){
 			std::cout << "-->the List is empty!<--";
@@ -123,30 +125,34 @@ public:
 		print();
 	}
 	void print(){
-		Node *temp;
-		temp = head;
-		int i = 0;
-		std::cout << "Number : \n";
-		while (temp){
-
-			std::cout << temp->element;
-			if (i < size - 1)
-				std::cout << "<--->";
-			temp = temp->next;
-			i++;
+		if (size == 0){
+			std::cout << "-->!list is empty!<--" << std::endl;
 		}
-		std::cout << std::endl;
+		else{
+			Node<T> *temp;
+			temp = head;
+			int i = 0;
+			std::cout << "Number : \n";
+			while (temp){
 
+				std::cout << temp->element;
+				if (i < size - 1)
+					std::cout << "<--->";
+				temp = temp->next;
+				i++;
+			}
+			std::cout << std::endl;
+		}
 	}
-	void insertFirst(Node *first){
+	void insertFirst(Node<T> *first){
 		insertAfter(1, first);
-		int temp = head->element;
+		T temp = head->element;
 		head->element = head->next->element;
 		head->next->element = temp;
 	}
 
-	void add(Node *node){
-		Node *temp = head;
+	void add(Node<T> *node){
+		Node<T> *temp = head;
 		addRecursive(temp, node);
 	}
 	void removeWithRecursive(){
@@ -157,7 +163,7 @@ public:
 			head = NULL;
 		}
 		else{
-			Node *temp = head;
+			Node<T> *temp = head;
 			recursiveRemove(temp);
 
 		}
@@ -168,12 +174,80 @@ public:
 
 	}
 };
-int main()
-{
-	system("color 4");
+void inputChar(){
 	int choice;
-	LinkedList obj;
+	LinkedList<char> obj;
+	char input;
+	char c;
+	while (1){
 
+		std::cout << "1_insert last \n2_remove \n3_insert after\n4_print\n5_insert first\n6_reverse\n7_add\n8_recursive remove\n";
+		std::cin >> choice;
+		if (choice == 1){
+			int size;
+			std::cout << "enter number of char : ";
+			std::cin >> size;
+
+
+			int i = 0;
+			while (i < size)
+			{
+				std::cout << "enter a char : ";
+				std::cin >> input;
+
+				Node<char> *node = new Node<char>(input);
+
+				obj.insertLast(node);
+				i++;
+
+			}
+			obj.print();
+
+		}
+		if (choice == 2){
+			obj.remove();
+		}
+		if (choice == 3){
+			int after;
+			std::cout << "after which number do you want to insert ? : ";
+			std::cin >> after;
+			std::cout << "enter a char :";
+
+			std::cin >> c;
+
+			Node<char> *node = new Node<char>(c);
+			obj.insertAfter(after, node);
+		}
+		if (choice == 4){
+			obj.print();
+		}
+		if (choice == 5){
+			std::cout << "enter a char :";
+
+			std::cin >> c;
+			Node<char> *first = new Node<char>(c);
+			obj.insertFirst(first);
+		}
+		if (choice == 6){
+			obj.reverse();
+		}
+		if (choice == 7){
+
+			std::cout << "Enter a char : ";
+			std::cin >> c;
+			Node<char> *node = new Node<char>(c);
+			obj.add(node);
+		}
+		if (choice == 8){
+			obj.removeWithRecursive();
+		}
+	}
+}
+void inputInt(){
+	int choice;
+	LinkedList<int> obj;
+	int input;
+	int number;
 	while (1){
 
 		std::cout << "1_insert last \n2_remove \n3_insert after\n4_print\n5_insert first\n6_reverse\n7_add\n8_recursive remove\n";
@@ -182,7 +256,7 @@ int main()
 			int size;
 			std::cout << "enter number of numbers : ";
 			std::cin >> size;
-			int input;
+
 
 			int i = 0;
 			while (i < size)
@@ -190,7 +264,7 @@ int main()
 				std::cout << "enter a number : ";
 				std::cin >> input;
 
-				Node *node = new Node(input);
+				Node<int> *node = new Node<int>(input);
 
 				obj.insertLast(node);
 				i++;
@@ -207,10 +281,10 @@ int main()
 			std::cout << "after which number do you want to insert ? : ";
 			std::cin >> after;
 			std::cout << "enter a number :";
-			int number;
+
 			std::cin >> number;
 
-			Node *node = new Node(number);
+			Node<int> *node = new Node<int>(number);
 			obj.insertAfter(after, node);
 		}
 		if (choice == 4){
@@ -218,25 +292,127 @@ int main()
 		}
 		if (choice == 5){
 			std::cout << "enter a number :";
-			int number;
+
 			std::cin >> number;
-			Node *first = new Node(number);
+			Node<int> *first = new Node<int>(number);
 			obj.insertFirst(first);
 		}
 		if (choice == 6){
 			obj.reverse();
 		}
 		if (choice == 7){
-			int number;
+
 			std::cout << "Enter a number : ";
 			std::cin >> number;
-			Node *node = new Node(number);
+			Node<int> *node = new Node<int>(number);
 			obj.add(node);
 		}
 		if (choice == 8){
 			obj.removeWithRecursive();
 		}
 	}
+}
+void inputString(){
+	int choice;
+	LinkedList<std::string> obj;
+	std::string input;
+	std::string  str;
+	while (1){
+
+		std::cout << "1_insert last \n2_remove \n3_insert after\n4_print\n5_insert first\n6_reverse\n7_add\n8_recursive remove\n";
+		std::cin >> choice;
+		if (choice == 1){
+			int size;
+			std::cout << "enter number of numbers : ";
+			std::cin >> size;
+
+
+			int i = 0;
+			while (i < size)
+			{
+				std::cout << "enter a string : ";
+				std::cin >> input;
+
+				Node<std::string> *node = new Node<std::string>(input);
+
+				obj.insertLast(node);
+				i++;
+
+			}
+			obj.print();
+
+		}
+		if (choice == 2){
+			obj.remove();
+		}
+		if (choice == 3){
+			int after;
+			std::cout << "after which number do you want to insert ? : ";
+			std::cin >> after;
+			std::cout << "enter a string :";
+
+			std::cin >> str;
+
+			Node<std::string> *node = new Node<std::string>(str);
+			obj.insertAfter(after, node);
+		}
+		if (choice == 4){
+			obj.print();
+		}
+		if (choice == 5){
+			std::cout << "enter a string :";
+
+			std::cin >> str;
+			Node<std::string> *first = new Node<std::string>(str);
+			obj.insertFirst(first);
+		}
+		if (choice == 6){
+			obj.reverse();
+		}
+		if (choice == 7){
+
+			std::cout << "Enter a string : ";
+			std::cin >> str;
+			Node<std::string> *node = new Node<std::string>(str);
+			obj.add(node);
+		}
+		if (choice == 8){
+			obj.removeWithRecursive();
+		}
+	}
+}
+void inputDouble(){
+
+}
+////////////////////////
+template <class T>
+int main()
+{
+	
+	system("color 4");
+	int choice;
+	std::cout << "what type of data do you want to put in list? \n1_char\n2_int\n3_string\n4_double";
+	int dataType;
+	std::cin >> dataType;
+	
+
+	if (dataType == 1){
+		inputChar();
+	}
+	else if (dataType == 2){
+		inputInt();
+	}
+	else if (dataType == 3){
+		inputString();
+		
+	}
+	else if (dataType == 4){
+		inputDouble();
+	}
+	
+
+
+
 	system("pause");
 	return 0;
 }
