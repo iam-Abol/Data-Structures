fd88629d6139ac11e73f24c42c3f68b088e97d58
iam-Abol <mh.z77w@gmail.com>
2020-07-09 21:07:12 +0430

    Node class for stack finished

---
+++ Stack/Stack/Stack.cpp
@@ -0,0 +1,19 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+class Node{
+public:
+	std::string element;
+	Node *next;
+
+	Node(std::string element){
+		this->element = element;
+	}
+	
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
