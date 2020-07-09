94709c0a4cb632223c143871a9c619114e9768be
iam-Abol <mh.z77w@gmail.com>
2020-07-09 21:19:59 +0430

    print function finished

--- Stack/Stack/Stack.cpp
+++ Stack/Stack/Stack.cpp
@@ -2,6 +2,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 class Node{
 public:
 	std::string element;
@@ -10,10 +11,41 @@ public:
 	Node(std::string element){
 		this->element = element;
 	}
-	
+	Node(){}
+};
+
+
+class Stack{
+	Node *head = new Node;
+	int size = 0;
+public:
+	void print(){
+		if (size == 0)
+			std::cout << "--> stack is empty <--" << std::endl;
+		else{
+			std::cout << "elements : \n";
+			Node *temp = head;
+			int i = 0;
+			while (temp){
+				std::cout << temp->element << std::endl;
+				temp = temp->next;
+				if (i < size - 1){
+					std::cout << " <-->";
+				}
+			}
+			std::cout << std::endl;
+		}
+
+	}
 };
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Stack obj;
+	obj.print();
+
+
+	std::cin.get();
+	std::cin.get();
 	return 0;
 }
 
