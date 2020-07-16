4fad492283f3199bf0cf4e286e7e4c8d5c8f9a29
iam-Abol <mh.z77w@gmail.com>
2020-07-16 18:29:50 +0430

    menu and print function finished

--- Queue/Queue/Queue.cpp
+++ Queue/Queue/Queue.cpp
@@ -4,9 +4,13 @@ class Node{
 public:
 	int element;
 	Node * next;
-	Node(){}
+	Node(){
+		next = NULL;
+		element = 0;
+	}
 	Node(int element){
 		this->element = element;
+		next = NULL;
 	}
 };
 class Queue{
@@ -23,9 +27,52 @@ public:
 	int getSize(){
 		return size;
 	}
+	
+	void print(){
+		bool flag = isEmpty();
+		if (flag == true)
+			std::cout << "--> Queue is empty <--" << std::endl;
+		else{
+
+			std::cout << "elements : " << std::endl;
+			Node*temp = head;
+			while (temp){
+				std::cout << temp->element << std::endl;
+				temp = temp->next;
+			}
+		}
+
+	}
 };
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int choice;
+	
+	
+	Queue obj;
+	std::cout << "1_size\n2_isEmpty\n3_print" << std::endl;
+	while (1){
+		std::cin >> choice;
+		if (choice == 1)
+			std::cout<<"size : "<<obj.getSize()<<std::endl;
+		else if (choice == 2){
+			bool flag = obj.isEmpty();
+			if (flag == true)
+				std::cout << "true" << std::endl;
+			else{
+				std::cout << "false" << std::endl;
+			}
+		}
+		else if (choice == 3)
+			obj.print();
+			
+	}
+
+
+
+
+	std::cin.get();
+	std::cin.get();
 	return 0;
 }
 
