d8fa8cc992281bd40ac26c538885961b30f725eb
iam-Abol <mh.z77w@gmail.com>
2020-07-16 16:57:53 +0430

    Node class finished

---
+++ Queue/Queue/Queue.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+class Node{
+public:
+	int element;
+	Node * next;
+
+	Node(int element){
+		this->element = element;
+	}
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
