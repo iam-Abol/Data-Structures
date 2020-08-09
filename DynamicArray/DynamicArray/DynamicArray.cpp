c5a4eb53d0b09138d8b1e3cf8c1481d52e784966
iam-Abol <mh.z77w@gmail.com>
2020-08-09 16:26:59 +0430

    print function finished

--- DynamicArray/DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -1,34 +1,43 @@
 #include "stdafx.h"
 #include <iostream>
-
-
-template <class Temp>
 struct variable
 {
-	Temp value;
+	int value;
 	bool isDeleted;
 	variable(){
 		value = 0;
 		isDeleted = false;
 	}
 };
-
-template <class Temp>
 class DynamicArray{
-	variable *Array ;
-	int sizeOfArray ;
+	variable *Array = new variable[10];
+	int sizeOfArray;
 	int sizeOfdata;
-public :
+public:
 	DynamicArray(){
-		Array = new variable[10];
 		sizeOfArray = 10;
 		sizeOfdata = 0;
 	}
-	
+	void print(){
+		if (sizeOfdata == 0){
+			std::cout << "--> Array is empty <--" << std::endl;
+		}
+		else{
+			for (int i = 0; i < sizeOfdata; i++){
+				if (Array[i].isDeleted == false)
+					std::cout << Array[i].value << std::endl;
+			}
+		}
+
+	}
 };
+
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	DynamicArray myArray;
+	myArray.print();
 
 	std::cin.get();
 	return 0;
