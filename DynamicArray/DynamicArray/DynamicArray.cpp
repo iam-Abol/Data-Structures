279667448a3c91920866ccd232162133892e26ca
iam-Abol <mh.z77w@gmail.com>
2020-08-09 15:06:09 +0430

    variable structure finished

---
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+template <class Temp>
+struct variable
+{
+	Temp value;
+	bool isDeleted;
+	variable(){
+		value = 0;
+		isDeleted = false;
+	}
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	
+	std::cin.get();
+	return 0;
+}
+
