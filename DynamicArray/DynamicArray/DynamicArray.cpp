50260c22ceeabe42b9fac1d59620b80308e76105
iam-Abol <mh.z77w@gmail.com>
2020-08-09 15:44:00 +0430

    DynamicArray class variables finished

--- DynamicArray/DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include <iostream>
+
+
 template <class Temp>
 struct variable
 {
@@ -10,10 +12,17 @@ struct variable
 		isDeleted = false;
 	}
 };
+
+template <class Temp>
+class DynamicArray{
+	variable *Array = new variable[10];
+	int size = 10;
+
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
-	
+
+
 	std::cin.get();
 	return 0;
 }
