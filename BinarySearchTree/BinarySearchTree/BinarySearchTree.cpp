8fa2e5d3e28dceaabf9e218ef85118961ab25f2a
iam-Abol <mh.z77w@gmail.com>
2020-10-05 09:44:47 +0330

    Node class variables finished

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+class Node{
+
+private:
+
+	int element;
+	Node *leftChild, *rightChild;
+
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	 
+	return 0;
+}
+
