57e258f69f28525ab005611bc361f9b99b7d25f9
iam-Abol <mh.z77w@gmail.com>
2020-12-06 14:14:17 +0330

    HashTable class added

--- hashTable/hashTable/hashTable.cpp
+++ hashTable/hashTable/hashTable.cpp
@@ -1,14 +1,21 @@
 #include "stdafx.h"
 #include <iostream>
 class Node{
+public:
 	int element;
 	Node *next;
+};
+class HashTable{
+	Node array[10];
+public:
+
 };
 int hash(int key, int size){
 	return key%size;
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
+
 	return 0;
 }
 
