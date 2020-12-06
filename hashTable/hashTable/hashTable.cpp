7abd5e3e4651e4099336e41e02b663b39aead77e
iam-Abol <mh.z77w@gmail.com>
2020-12-06 14:25:00 +0330

    constructore added to Node class

--- hashTable/hashTable/hashTable.cpp
+++ hashTable/hashTable/hashTable.cpp
@@ -4,13 +4,25 @@ class Node{
 public:
 	int element;
 	Node *next;
+	Node(){
+		element = 0;
+		next = NULL;
+	}
 };
 class HashTable{
 	Node array[10];
 public:
+	HashTable(){
 
+	}
+	void chaining(int newNumber){
+		int index = hash(newNumber, 10);
+		if (array[index] == NULL){
+
+		}
+	}
 };
-int hash(int key, int size){
+int hash(int key, int size = 10){
 	return key%size;
 }
 int _tmain(int argc, _TCHAR* argv[])
