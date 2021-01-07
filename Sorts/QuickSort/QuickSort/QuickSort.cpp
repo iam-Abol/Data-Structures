9cb12cace25a2966d02c30d5657e01718c21c4dd
iam-Abol <mh.z77w@gmail.com>
2021-01-07 10:33:02 +0330

    quickSort changed

--- Sorts/QuickSort/QuickSort/QuickSort.cpp
+++ Sorts/QuickSort/QuickSort/QuickSort.cpp
@@ -3,12 +3,33 @@
 void print(int *number, int size){
 	for (size_t i = 0; i < size; i++)
 	{
-		std::cout << number[i] << std::endl;
+		std::cout << number[i] << " ";
 	}
+	std::cout << std::endl;
 }
-void quickSort(int number[],int size, int start, int end){
-	
-
+void swap(int &a, int &b){
+	int t = a;
+	a = b;
+	b = t;
+}
+void quickSort(int *number, int size, int start, int end){
+	int p = start;
+	int j = end - 1;
+	for (int i = start; i <= j; i++)
+	{
+		if (number[i] > number[p]){
+			while (number[j] > number[p]){
+				j--;
+				if (i == j)
+					break;
+			}
+			if (i != j&&i<j)
+				swap(number[i], number[j]);
+		}
+		
+	}
+	swap(number[p], number[j - 1]);
+	print(number, size);
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -19,7 +40,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < n; i++)
 		std::cin >> number[i];
 
-	quickSort(number,n ,0, n - 1);
+	quickSort(number, n, 0, n);
 
 	delete[] number;
 	return 0;
