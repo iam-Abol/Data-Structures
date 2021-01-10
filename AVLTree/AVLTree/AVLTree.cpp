982d59e98e970e367b4bc0ce79053e9d049ccbec
iam-Abol <mh.z77w@gmail.com>
2021-01-10 09:19:35 +0330

    height and balanceFactor added to Node class

--- AVLTree/AVLTree/AVLTree.cpp
+++ AVLTree/AVLTree/AVLTree.cpp
@@ -3,12 +3,18 @@
 class Node{
 	int element;
 	Node *leftChild, *rightChild;
+	int height, balanceFactor;
 public:
 	Node(int element){
 		this->element = element;
 	}
 };
+class AVLTree{
+private:
 
+public:
+
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
 	Node n(10);
